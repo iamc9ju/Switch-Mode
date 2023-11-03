ea96153503633c2cf7d42a79edf040f4182c8e83
Ittipol Botmoon <ittipolbotmoon@Ittipols-Air.true.true>
2023-11-04 02:10:46 +0700

    update

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const icon = document.querySelector("i");
-const text = document.querySelector("h2");
-const body = document.querySelector("body");
+const text = document.querySelector('h2');
+const body = document.querySelector('body');
 
-icon.addEventListener("click",function(){
+icon.addEventListener('click',function(){
     this.classList.toggle("bi-moon-stars-fill");
     if(this.classList.toggle("bi-brightness-high-fill")){
-        text.innerText ="โหมดกลางวัน";
-        body.style.background="white";
-        body.style.color="Black"
-        body.style.transition="1s";
+        
+        text.innerText = "โหมดกลางวัน";
+        body.style.background="White";
+        body.style.color = "Black";
+        body.style.transition ="1s";
     }else{
-        text.innerText ="โหมดกลางคืน";
-        body.style.background="Black";
-        body.style.color="White"
-        body.style.transition="1s";
+        text.innerText = "โหมดกลางคืน";
+        body.style.background = "Black";
+        body.style.color = "White";
+        body.style.transition = "1s";
     }
 })
